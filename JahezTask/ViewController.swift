efa86b6addbefd4d43b361e21b795c40272ba20f
Satham Hussain <sain.96@gmail.com>
2022-01-25 22:40:51 +0300

    Initial Commit

---
+++ JahezTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JahezTask
+//
+//  Created by Satham Hussain on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
